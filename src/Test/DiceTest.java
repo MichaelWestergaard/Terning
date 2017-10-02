00f98db0322d07988df59334e1aa5efd7d8f7fbe
Michael <Michael@Michael-Laptop.clients.wireless.dtu.dk>
2017-10-02 16:26:26 +0200

    Added JUnit Test

---
+++ src/Test/DiceTest.java
@@ -0,0 +1,31 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DiceTest {
+
+	//hh
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testRoll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRollMultiple() {
+		fail("Not yet implemented");
+	}
+
+}
